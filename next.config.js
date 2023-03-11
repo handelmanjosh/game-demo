12cbf9a863debddc66e8ada2df1278d10b8baba7
josh handelman <71081051+handelmanjosh@users.noreply.github.com>
2023-03-11 13:58:47 -0500

    fuck you

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-export const reactStrictMode = false;
-export const eslint = { ignoreDuringBuilds: true };
-export const typescript = {
-  // !! WARN !!
-  // Dangerously allow production builds to successfully complete even if
-  // your project has type errors.
-  // !! WARN !!
-  ignoreBuildErrors: true,
-};
 
+const nextConfig = {
+  reactStrictMode: true,
+    
+}
 
-
-
+module.exports = nextConfig
